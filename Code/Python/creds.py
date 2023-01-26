666d531b5cb5f1f29e2fe37909e695205909beb4
Zan Zver <32227026+ZanZver@users.noreply.github.com>
2023-01-26 16:28:51 +0000

    Delete creds.py

--- Code/Python/creds.py
+++
@@ -1,5 +0,0 @@
-bot_username = "ThisMightBeEDUBot"
-bot_pass = "cUw*j#@B&iV2e2&PN#BScpwRx8LMiR$9ee98v@i9"
-bot_ID = "iJPf7lw8B7vnZZFVz-9ijg"
-bot_token = "7ShdmdF6rhPm4JfZmODhZ67TMxrt5g"
-bot_g_type = "password"
